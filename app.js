97777c3a1dea3ea2d6192139a87dd917f430ae7c
Andre Garcia <andreemmanuelgarcia@gmail.com>
2022-10-12 10:23:16 +0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const session = require('express-session');
+const bodyParser = require("body-parser");
+const app = express();
+
+const PORT = 8000;
+app.use(express.static(__dirname + "/"));
+
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(bodyParser.urlencoded({extended: true, limit: "10mb"}));
+
+const router = require("./02-pokeDex/router.js");
+app.use("/api", router);
+
+app.listen(PORT, () => console.log("listening at " + PORT));
